b78f238be9a307d4987e528049b40cdae0b53ce1
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-11-09 02:09:24 +0900

    :sparkles: Add the index.ts app.ts

---
+++ backend/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+import flowController from "./controller/flowController";
+
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("hello express\n");
+});
+
+app.use("/users", flowController);
+
+export default app;
