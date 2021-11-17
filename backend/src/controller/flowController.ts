506e50d9b07dea87daa3d3c1d66568c97a47470d
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-11-17 13:27:59 +0900

    :recycle: Add the try catch

--- backend/src/controller/flowController.ts
+++ backend/src/controller/flowController.ts
@@ -1,130 +1,163 @@
-import { Path, PrismaClient } from "@prisma/client";
-import { Request, Response,Router } from "express";
+import { PrismaClient } from '@prisma/client';
+import { Request, Response, Router } from 'express';
 
 const prisma = new PrismaClient();
 const router = Router();
 
 // GET /flows
-router.get("/", async (req: Request, res: Response) => {
-  const flows = await prisma.flow.findMany();
-  res.status(200).json(flows);
+router.get('/', async (req: Request, res: Response) => {
+  try {
+    const flows = await prisma.flow.findMany();
+    res.status(200).json(flows);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+// GET /flows/:id
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const flow = await prisma.flow.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
+    if (!flow) {
+      res.status(400).send(`There is no flow with id ${req.params?.id}`);
+      return;
+    }
+    res.status(200).json(flow);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
 });
 
 // POST /flows
-router.post("/", async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const flow = await prisma.flow.create({
-    data: {
-      name: name
-    }
-  });
-  if (flow) {
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const flow = await prisma.flow.create({
+      data: {
+        name: name,
+      },
+    });
     res.status(201).json(flow);
-  } else {
+  } catch (e) {
+    console.log(e);
     res.status(500).send();
   }
-})
+});
 
 // DELETE /flows/{flow_id}
-router.delete("/:id", async (req: Request, res: Response) => {
-  const flow = await prisma.flow.delete({
-    where: { id: parseInt(req.params?.id) },
-  });
-  if (flow) {
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const flow = await prisma.flow.delete({
+      where: { id: parseInt(req.params?.id) },
+    });
+
     res.status(200).json(flow);
-  } else {
+  } catch (e) {
+    console.log(e);
     res.status(500).send();
   }
-})
+});
 
 // PUT /flows/{flow_id}
-router.put("/:id", async (req: Request, res: Response) => {
-  const { name } = req.body;
-  const flow = await prisma.flow.findUnique({
-    where: { id: parseInt(req.params?.id) },
-  });
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const { name } = req.body;
+    const flow = await prisma.flow.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
 
-  if (!flow) {
-    throw new Error("flow is empty")
-  }
-
-  const updated_flow = await prisma.flow.update({
-    where: {
-      id: parseInt(req.params?.id)
-    },
-    data: {
-      name: name
+    if (!flow) {
+      res.status(400).send(`There is no flow with id ${req.params?.id}`);
+      return;
     }
-  })
 
-  if (updated_flow) {
-    res.status(200).json({ updated_flow })
-  } else {
+    const updated_flow = await prisma.flow.update({
+      where: {
+        id: parseInt(req.params?.id),
+      },
+      data: {
+        name: name,
+      },
+    });
+
+    res.status(200).json(updated_flow);
+  } catch (e) {
+    console.log(e);
     res.status(500).send();
   }
-
 });
 
 // GET /flows/{flow_id}/tasks
-router.get("/:id/tasks", async (req: Request, res: Response) => {
-  const flow = await prisma.flow.findUnique({
-    where: { id: parseInt(req.params?.id) },
-  });
-  if (!flow) {
-    throw new Error("flow is empty")
-  }
-  const tasks = await prisma.task.findMany({
-    where: {
-      flowId: flow.id,
+router.get('/:id/tasks', async (req: Request, res: Response) => {
+  try {
+    const flow = await prisma.flow.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
+    if (!flow) {
+      res.status(400).send(`There is no flow with id ${req.params?.id}`);
+      return;
     }
-  });
-  res.status(200).json(tasks);
+    const tasks = await prisma.task.findMany({
+      where: {
+        flowId: flow.id,
+      },
+    });
+    res.status(200).json(tasks);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
 });
 
 // GET /flows/{flow_id}/paths
-router.get("/:id/paths", async (req: Request, res: Response) => {
-  const flow = await prisma.flow.findUnique({
-    where: { id: parseInt(req.params?.id) },
-  });
-  if (!flow) {
-    throw new Error("flow is empty")
+router.get('/:id/paths', async (req: Request, res: Response) => {
+  try {
+    const flow = await prisma.path.findUnique({
+      where: {
+        id: parseInt(req.params?.id),
+      },
+    });
+    if (!flow) {
+      res.status(400).send(`There is no flow with id ${req.params?.id}`);
+      return;
+    }
+
+    const paths = await prisma.path.findMany({
+      where: {
+        flowId: flow.id,
+      },
+    });
+    res.status(200).json(paths);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
   }
-
-  const paths = await prisma.path.findMany();
-  const tasks = await prisma.task.findMany({
-    where: {
-      flowId: flow.id,
-    }
-  });
-
-  (async () => {
-    const related_paths: Path[] = [];
-    for (const task of tasks) {
-      for (const path of paths) {
-        if (task.id === path.fromTaskId) {
-          related_paths.push(path)
-        }
-      }
-    }
-    res.status(200).json( related_paths );
-  })()
 });
 
 // GET /flows/{flow_id}/actors
-router.get("/:id/actors", async (req: Request, res: Response) => {
-  const flow = await prisma.flow.findUnique({
-    where: { id: parseInt(req.params?.id) },
-  });
-  if (!flow) {
-    throw new Error("flow is empty")
-  }
-  const actors = await prisma.actor.findMany({
-    where: {
-      flowId: flow.id,
+router.get('/:id/actors', async (req: Request, res: Response) => {
+  try {
+    const flow = await prisma.flow.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
+    if (!flow) {
+      res.status(400).send(`There is no flow with id ${req.params?.id}`);
+      return;
     }
-  });
-  res.status(200).json(actors);
+    const actors = await prisma.actor.findMany({
+      where: {
+        flowId: flow.id,
+      },
+    });
+    res.status(200).json(actors);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
 });
 
-
-export default router
+export default router;
