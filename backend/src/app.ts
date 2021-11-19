1072325e911c5d22a19acd9e93b5b3cfb384c696
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-11-19 19:19:49 +0900

    :recycle: use prettier

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,10 @@
-import express from "express";
+import express from 'express';
 
-import flowController from "./controller/flowController";
-
+import flowController from './controller/flowController';
 
 const app = express();
 app.use(express.json());
 
-app.use("/flows", flowController);
+app.use('/flows', flowController);
 
 export default app;
