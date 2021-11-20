44a02ba37e165a94c1cec7b0389751f191f344ea
DuGlaser <duglaser0618@gmail.com>
2021-11-20 22:17:13 +0900

    :sparkles: Add the task controller.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 
 import flowController from './controller/flowController';
+import taskController from './controller/taskController';
 
 const app = express();
 app.use(express.json());
 
 app.use('/flows', flowController);
+app.use('/tasks', taskController);
 
 export default app;
---
+++ backend/src/controller/taskController.ts
@@ -0,0 +1,107 @@
+import { PrismaClient } from '@prisma/client';
+import { Request, Response, Router } from 'express';
+
+const prisma = new PrismaClient();
+const router = Router();
+
+// POST /tasks
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const {
+      flow_id: flowId,
+      name,
+      task_id: typeId,
+      actor_id: actorId,
+    } = req.body;
+
+    const tasks = await prisma.task.create({
+      data: {
+        flowId,
+        name,
+        typeId,
+        actorId,
+      },
+    });
+    res.status(200).send(tasks);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+// GET /tasks/{task_id}
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const task = await prisma.task.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
+
+    if (!task) {
+      res.status(400).send(`There is no task with id ${req.params?.id}`);
+      return;
+    }
+
+    res.status(200).send(task);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+// DELETE /tasks/{task_id}
+router.delete('/:id', async (req: Request, res: Response) => {
+  try {
+    const task = await prisma.task.delete({
+      where: { id: parseInt(req.params?.id) },
+    });
+
+    res.status(200).json(task);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+// PUT /tasks/{task_id}
+router.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const { name, task_id: typeId, actor_id: actorId } = req.body;
+    const task = await prisma.task.findUnique({
+      where: { id: parseInt(req.params?.id) },
+    });
+
+    if (!task) {
+      res.status(400).send(`There is no task with id ${req.params?.id}`);
+      return;
+    }
+
+    const actor = await prisma.actor.findUnique({
+      where: {
+        id: actorId,
+      },
+    });
+
+    if (!actor || actor?.flowId !== task.flowId) {
+      res.status(400).send(`actor_id is invalid.`);
+      return;
+    }
+
+    const updatedFlow = await prisma.task.update({
+      where: {
+        id: parseInt(req.params?.id),
+      },
+      data: {
+        name,
+        typeId,
+        actorId,
+      },
+    });
+
+    res.status(200).json(updatedFlow);
+  } catch (e) {
+    console.log(e);
+    res.status(500).send();
+  }
+});
+
+export default router;
