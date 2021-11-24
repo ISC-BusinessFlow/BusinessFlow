cba4e48643acd2b730f825f0f7fcfeab371099e2
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2021-11-25 01:14:11 +0900

    ✨ Add the actor controller.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,11 @@
 import express from 'express';
-
+import actorController from './controller/actorController';
 import flowController from './controller/flowController';
 
 const app = express();
 app.use(express.json());
 
 app.use('/flows', flowController);
+app.use('/actors', actorController);
 
 export default app;
