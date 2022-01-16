2c150b2b4eb94c5671da942a8ad0070641f9d3ae
N0s3nsE <N0s3nsE.NNS@gmail.com>
2022-01-16 22:57:31 +0900

    add task component

---
+++ frontend/src/components/Task/displaytext.tsx
@@ -0,0 +1,25 @@
+import { observer } from 'mobx-react-lite';
+
+import { TaskType } from '@/lib/Task';
+
+export const DisplayText: React.VFC<{
+  task: TaskType;
+  height: number;
+  width: number;
+}> = observer(({ task, width, height }) => {
+  return (
+    <foreignObject
+      x={task.x}
+      y={task.y}
+      width={width}
+      height={height}
+      fill="none"
+    >
+      <html lang="jp">
+        <div style={{ textAlign: 'center', padding: '4px' }}>
+          <span>{task.label}</span>
+        </div>
+      </html>
+    </foreignObject>
+  );
+});
--- frontend/src/components/Task/index.tsx
+++ frontend/src/components/Task/index.tsx
@@ -4,8 +4,22 @@ import { useRef } from 'react';
 import { useRegisterNode } from '@/diagrams';
 import { Task as TaskType } from '@/lib/Task';
 
+import { Process } from './process';
+import { Trigger } from './trigger';
+
 export const Task: React.VFC<{ task: TaskType }> = observer(({ task }) => {
   const taskRef = useRef<SVGRectElement>(null);
   useRegisterNode(taskRef, task.id);
-  return <rect ref={taskRef} x={task.x} y={task.y} width="40" height="40" />;
+
+  //toriaezu
+  switch (task.type) {
+    case 'trigger':
+      return <Trigger task={task} />;
+    case 'process':
+      return <Process task={task} />;
+    default:
+      return (
+        <rect ref={taskRef} x={task.x} y={task.y} width="40" height="40" />
+      );
+  }
 });
