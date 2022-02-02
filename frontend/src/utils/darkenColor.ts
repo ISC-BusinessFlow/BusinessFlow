b692105b0338902b222e668943d00d04801b2c6b
N0s3nsE <N0s3nsE.NNS@gmail.com>
2022-02-02 10:53:07 +0900

    fix

--- frontend/src/utils/darkenColor.ts
+++ frontend/src/utils/darkenColor.ts
@@ -1,13 +1,13 @@
-export const darkenColor = (color: string, degree: number): string => {
+export const darkenColor = (color: string, diff: number): string => {
   const colorCode = color.slice(1, 7).match(/.{2}/g);
   if (colorCode === null) {
     return '#000000';
   }
   const borderColorCode = colorCode.map((c) => {
-    if (parseInt(c, 16) < degree) {
+    if (parseInt(c, 16) < diff) {
       return '00';
     } else {
-      return (parseInt(c, 16) - degree).toString(16).padStart(2, '0');
+      return (parseInt(c, 16) - diff).toString(16).padStart(2, '0');
     }
   });
   return '#' + borderColorCode.join('');
