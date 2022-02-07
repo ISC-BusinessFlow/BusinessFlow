14cf77bf8ed4707b54d7eef35914b733a79cedae
tk84 <bjjt1290058@gn.iwasaki.ac.jp>
2022-02-07 09:52:43 +0900

    :recycle: no-non-null-assertion

--- backend/src/corsOptions.ts
+++ backend/src/corsOptions.ts
@@ -4,20 +4,20 @@ const env =
   process.env.NODE_ENV !== undefined ? process.env.NODE_ENV : 'development';
 
 export const accessPort = () => {
-  const port = accessOrigin()?.split(':').slice(-1)[0];
-  return parseInt(port!);
+  const port = accessOrigin().split(':').slice(-1)[0];
+  return parseInt(port);
 };
 
 export const accessOrigin = () => {
   switch (env) {
     case 'development':
-      return process.env.DEVELOPMENT_URL;
+      return process.env.DEVELOPMENT_URL || '';
     case 'production':
-      return process.env.PRODUCTION_URL;
+      return process.env.PRODUCTION_URL || '';
     case 'test':
-      return process.env.TEST_URL;
+      return process.env.TEST_URL || '';
     default:
-      return process.env.DEVELOPMENT_URL;
+      return process.env.DEVELOPMENT_URL || '';
   }
 };
 
